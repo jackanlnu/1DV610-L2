da219704f0fe4591f6462a797d06094a48ec124c
Jack <jo223se@student.lnu.se>
2025-09-15 16:23:29 +0200

    Fix sorting functions index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Sorting Functions
 function sortAlphabet(arr){
-  return arr.sort()
+  return [...arr].sort()
 }
 
-//console.log(sortAlphabet(['a','c','b','A','1'])) 
+function sortValue(arr){
+  return [...arr].sort((a, b) => a - b)
+}
+
+// console.log(sortValue([0,5,1,17,200,2])) 
+// console.log(sortAlphabet(['a','c','b','A','1'])) 
