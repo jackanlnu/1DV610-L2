60dd059583a1236445f7f4f9ba5b8914ddf76b3f
Jack <jo223se@student.lnu.se>
2025-09-17 14:08:26 +0200

    Add removeDuplicates function

--- index.js
+++ index.js
@@ -7,5 +7,25 @@ function sortValue(arr){
   return [...arr].sort((a, b) => a - b)
 }
 
+//
+function removeDuplicates(arr){
+	const uniqueArr = [arr[0]]
+	for (let i = 0; i < arr.length - 1; i++) {
+		let newUnique = true
+		for (let j = 0; j <= uniqueArr.length - 1; j++) {
+			if (arr[i] === uniqueArr[j]) {
+				newUnique = false
+			}
+		}
+		if(newUnique){
+			uniqueArr.push(arr[i])
+		}
+	}
+	return uniqueArr
+}
+
 // console.log(sortValue([0,5,1,17,200,2])) 
-// console.log(sortAlphabet(['a','c','b','A','1'])) 
+// console.log(sortAlphabet(['a','c','b','A','1']))
+//console.log(removeDuplicates(['a','c','b','A','1','1','1','a','c','c','c','c','c','A','c']))
+console.log(removeDuplicates(['1','1','1','3','2','2','3','3','2','1','3']))
+
