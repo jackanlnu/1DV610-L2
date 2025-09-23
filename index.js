39d5277b0e94d44e59f27f251e2d019e57be912d
Jack <jo223se@student.lnu.se>
2025-09-23 12:34:34 +0200

    Add test file

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ function removeDuplicates(arr){
 	return uniqueArr
 }
 
-// console.log(sortValue([0,5,1,17,200,2])) 
-// console.log(sortAlphabet(['a','c','b','A','1']))
-//console.log(removeDuplicates(['a','c','b','A','1','1','1','a','c','c','c','c','c','A','c']))
-console.log(sortCharLength(['Cdjwui','Hej','1','abceeeee','dedede']))
+export {
+  sortAlphabet,
+  sortValue,
+  sortCharLength,
+	removeDuplicates
+}
